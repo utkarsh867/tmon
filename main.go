755dfb2024938c52a95061458924c2da486b2b7a
Utkarsh Goel <hi@utkarshgoel.dev>
2024-02-06 22:16:12 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	wlog "github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	"github.com/utkarsh867/tmon/pkg/views"
+	"github.com/muesli/termenv"
+)
+
+type MainView struct {
+  leftLog tea.Model
+  rightLog tea.Model
+  pty ssh.Pty
+}
+
+func (m MainView) Init() tea.Cmd {
+  return tea.Batch(
+    m.leftLog.Init(),
+    m.rightLog.Init(),
+  )
+}
+
+func (m MainView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+  var (
+    cmds []tea.Cmd
+  )
+  switch msg := msg.(type) {
+  case tea.KeyMsg:
+    if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+      return m, tea.Quit
+    }
+  default:
+    var (
+      cmd1 tea.Cmd
+      cmd2 tea.Cmd
+    )
+    m.leftLog, cmd1 = m.leftLog.Update(msg)
+    m.rightLog, cmd2 = m.rightLog.Update(msg)
+    cmds = append(cmds, cmd1, cmd2)
+  }
+  return m, tea.Batch(cmds...)
+}
+
+func (m MainView) View() string {
+  return lipgloss.JoinHorizontal(
+    lipgloss.Left,
+    m.leftLog.View(),
+    m.rightLog.View(),
+  )
+}
+
+func main() {
+  f, err := tea.LogToFile("debug.log", "debug")
+  if err != nil {
+		log.Println("fatal:", err)
+		os.Exit(1)
+	}
+  defer f.Close()
+
+  p := func(s ssh.Session) *tea.Program {
+    pty, _, active := s.Pty()
+		if !active {
+			wish.Fatalln(s, "no active terminal, skipping")
+			return nil
+		}
+
+    initialModel := MainView{
+      leftLog: views.CreateLogModel(pty),
+      rightLog: views.CreateLogModel(pty),
+      pty: pty,
+    } 
+    prog := tea.NewProgram(initialModel, append(bm.MakeOptions(s), tea.WithAltScreen(), tea.WithMouseCellMotion())...)
+
+    return prog
+  }
+
+  s, err := wish.NewServer(
+    wish.WithAddress(fmt.Sprintf("%s:%d", "localhost", 23234)),
+    wish.WithMiddleware(
+      bm.MiddlewareWithProgramHandler(p, termenv.ANSI256),
+    ),
+  )
+
+  done := make(chan os.Signal, 1)
+  signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+  wlog.Info("Starting SSH server", "host", "localhost", "port", 23234)
+
+  go func() {
+    if err := s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed){
+      wlog.Error("Could not start server", "error", err)
+      done <- nil
+    }
+  }()
+  <-done
+
+  wlog.Info("Stopping SSH server")
+  ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+  defer func() { cancel() }()
+  if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		wlog.Error("could not stop server", "error", err)
+	}
+
+}
---
+++ pkg/views/logview.go
@@ -0,0 +1,115 @@
+package views
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/ssh"
+)
+
+type LogView struct {
+  sub chan LogStreamMessage
+  content string
+  ready bool
+  viewport viewport.Model
+  pty ssh.Pty
+}
+
+type LogStreamMessage struct {
+  content string
+}
+
+type LogStreamEndMessage struct {}
+
+
+func CreateLogModel(pty ssh.Pty) LogView {
+  content := ""
+  return LogView {
+    content: string(content),
+    pty: pty,
+    sub: make(chan LogStreamMessage),
+  }
+}
+
+func (m LogView) Init() tea.Cmd {
+  return tea.Batch(
+    m.waitForNewStreamLog(),
+    m.runStreamLog(),
+  )
+}
+
+func (m LogView) View() string {
+  if !m.ready {
+    return "\n Initializing..."
+  }
+
+  return fmt.Sprintf("%s\n", m.viewport.View())
+}
+
+func (m LogView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+  var (
+    cmd  tea.Cmd
+    cmds []tea.Cmd
+  )
+  switch msg := msg.(type) {
+  case tea.KeyMsg:
+    if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+      return m, tea.Quit
+    }
+  case LogStreamMessage:
+    newContent := msg.content
+    m.content = m.content + newContent
+    m.viewport.SetContent(m.content)
+    m.viewport.GotoBottom()
+  case tea.WindowSizeMsg:
+    if !m.ready {
+      m.viewport = viewport.New(msg.Width/3, msg.Height)
+      m.viewport.YPosition = 0
+      m.viewport.HighPerformanceRendering = false
+      m.viewport.SetContent(m.content)
+      m.ready = true
+    } else {
+      m.viewport.Width = m.pty.Window.Width / 3
+      m.viewport.Height = m.pty.Window.Height
+    }
+  }
+
+  m.viewport, cmd = m.viewport.Update(msg)
+  cmds = append(cmds, cmd, m.waitForNewStreamLog())
+  return m, tea.Batch(cmds...)
+}
+
+func (m LogView) runStreamLog() tea.Cmd {
+  return func() tea.Msg {
+    logs := exec.Command("log", "stream")
+    reader, err := logs.StdoutPipe()
+    if err != nil {
+      log.Fatal("Error")
+    }
+
+    err = logs.Start()
+    if err != nil {
+      log.Fatal("Error")
+    }
+
+    buf := make([]byte, 1024)
+    for {
+      n, err := reader.Read(buf)
+      if err != nil {
+        break
+      }
+      m.sub <- LogStreamMessage{content: string(buf[0:n])}
+    }
+    return LogStreamEndMessage{}
+  }
+}
+
+func (m *LogView) waitForNewStreamLog() tea.Cmd {
+  return func() tea.Msg {
+    content := <- m.sub
+    return content
+  }
+}
